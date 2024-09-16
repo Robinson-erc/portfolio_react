e72216bc71b038a95ebac4436b97c6ed8a656333
EricPC <eric.robinson1128@gmail.com>
2024-09-15 20:16:20 -0400

    edited skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -51,7 +51,7 @@ export const Skills = () => {
                                 </div>
                                 <div className="item">
                                     <img src={meter1} alt="Image" />
-                                    <h5>Web Development</h5>
+                                    <h5>Data Structures and Algorithms</h5>
                                 </div>
                             </Carousel>
                         </div>
